86d9b9d872c104e386aa64b7d277cb8b3efb1ccf
Diederik de Haas <github@cknow.org>
2018-03-12 06:08:15 +0100

    Removed (another) USD ID as it was already defined as default.
    Thanks again to MrEngman. See issue #47.

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -187,7 +187,6 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x056E, 0x400E),.driver_info = RTL8821}, /* ELECOM -  ELECOM */
 	{USB_DEVICE(0x056E, 0x400F),.driver_info = RTL8821}, /* ELECOM -  ELECOM */
 	{USB_DEVICE(0x0846, 0x9052),.driver_info = RTL8821}, /* Netgear - A6100 */
-	{USB_DEVICE(0x0BDA, 0xA811),.driver_info = RTL8821}, /* OUTLINK - Edimax */
 	{USB_DEVICE(0x0E66, 0x0023),.driver_info = RTL8821}, /* HAWKING - Edimax */
 	{USB_DEVICE(0x2001, 0x3314),.driver_info = RTL8821}, /* D-Link - Cameo */
 	{USB_DEVICE(0x2001, 0x3318),.driver_info = RTL8821}, /* D-Link - Cameo */
