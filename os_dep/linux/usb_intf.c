69b80089855879e18924bed40ffaec2d5e3f2c05
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-03-24 15:35:26 -0400

    Added Tenda u12 to UBS IDs

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -169,6 +169,7 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(0x20F4, 0x805B),.driver_info = RTL8812}, /* TRENDnet - */
 	{USB_DEVICE(0x2357, 0x0101),.driver_info = RTL8812}, /* TP-Link - Archer T4U */
 	{USB_DEVICE(0x2357, 0x0103),.driver_info = RTL8812}, /* TP-Link - T4UH */
+	{USB_DEVICE(0x2604, 0x0012),.driver_info = RTL8812}, /* Tenda - U12 */
 	{USB_DEVICE(0x7392, 0xA822),.driver_info = RTL8812}, /* Edimax - EW-7822UAC */
 
 #endif
