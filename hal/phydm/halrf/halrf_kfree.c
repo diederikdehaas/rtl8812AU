4f20da118fa1a7ae8c09f1279198ceae21ab2143
Diederik de Haas <github@cknow.org>
2019-08-10 17:15:45 +0200

    Fix 'misleading-indentation' warning.

--- hal/phydm/halrf/halrf_kfree.c
+++ hal/phydm/halrf/halrf_kfree.c
@@ -576,7 +576,7 @@ void	*p_dm_void)
 		else
 			p_power_trim_info->thermal = (pg_therm >> 1);
 
-			p_power_trim_info->flag |= KFREE_FLAG_THERMAL_K_ON;
+		p_power_trim_info->flag |= KFREE_FLAG_THERMAL_K_ON;
 	}
 
 	ODM_RT_TRACE(p_dm, ODM_COMP_MP, ODM_DBG_LOUD, ("[kfree] 8710b thermal trim flag:0x%02x\n", p_power_trim_info->flag));
