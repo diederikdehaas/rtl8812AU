d11833560484b39212b4e2a79c4ee071332badc0
Diederik de Haas <github@cknow.org>
2019-08-23 03:55:00 +0200

    Add support for kernels >= 4.20.
    Check is on 4.18 as the replacement function was then added.
    See https://github.com/torvalds/linux/commit/fb7fcc96a84c and
    https://github.com/zebulon2/rtl8814au/pull/2 and
    https://github.com/torvalds/linux/commit/dc1b7b6ca9d5
    Actually removed in kernel 4.20 with
    https://github.com/torvalds/linux/commit/976516404ff3

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -347,7 +347,11 @@ rtw_cfg80211_default_mgmt_stypes[NUM_NL80211_IFTYPES] = {
 
 static u64 rtw_get_systime_us(void)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0))
+	struct timespec64 ts;
+	ktime_get_boottime_ts64(&ts);
+	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
 	return ((u64)ts.tv_sec*1000000) + ts.tv_nsec / 1000;
